31f19e9c9028bb6bda4e8e6684524976b566b3b2
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-08-01 04:21:54 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-Console.BackgroundColor=ConsoleColor.White;
-Console.ForegroundColor=ConsoleColor.Black;
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ceditor 
+{
+    private static String source = " ";
+    public static void saves(String names,String a) 
+    {
+        File.WriteAllText(names,a);
+    
+    
+    }
+    public static void appends(String a) 
+    {
+        source = source + a;
+    
+    
+    }
+    public static String gets()
+    {
+        String a = "";
+        a = Console.ReadLine() ;
+        return a;
+
+
+    }
+    public static void editorloop(String names) 
+    {
+        source = "";
+        while (true)
+        {
+            String a = "";
+            a = gets();
+            appends(a+ "\r\n");
+            if (a == "") break;
+        }
+        saves(names, source);
+    
+    }
+}
+
+
+class coneditor {
+
+    public static void Main(String[] args)
+    {
+        String s = "";
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        
+        Console.WriteLine("give a file name to create ? empty line to save and exit ? ");
+        s= Console.ReadLine();
+        ceditor.editorloop(s);
+    }
+
+
+
+}
+
+
